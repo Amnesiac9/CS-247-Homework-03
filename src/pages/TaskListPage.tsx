01feca44b016bc4acfdaab8dcf8d5c38e46f6d23
John <johnmoreau19@gmail.com>
2024-05-04 14:13:18 -0700

    Scaffolding

---
+++ src/pages/TaskListPage.tsx
@@ -0,0 +1,18 @@
+
+function TaskListPage() {
+
+    return (
+        <>
+            <h1>Task List</h1>
+            <form>
+                <input type='text' placeholder="Enter a new task"></input>
+                <button>Create</button>
+            </form>
+            <div>
+                <p>task 1</p>
+            </div>
+        </>
+    )
+}
+
+export default TaskListPage
