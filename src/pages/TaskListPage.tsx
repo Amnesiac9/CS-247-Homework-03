edb4892c5ec00416b69bd2cf3c2f306058ccc389
John <johnmoreau19@gmail.com>
2024-05-04 15:13:01 -0700

    Added barebones task list

--- src/pages/TaskListPage.tsx
+++ src/pages/TaskListPage.tsx
@@ -1,15 +1,64 @@
+import React, { useEffect } from "react"
+import iconTrashCan from '../assets/iconmonstr-trash-can-thin.svg.svg'
+
+
+interface Task {
+    text: string
+    done: boolean
+}
 
 function TaskListPage() {
+
+    const [taskList, updateTaskList] = React.useState<Task[]>([])
+    const [inputValue, setInputValue] = React.useState('');
+    const inputRef = React.useRef<HTMLInputElement>(null);
+
+    function focusInput() {
+        if (inputRef.current) {
+            inputRef.current.focus();
+        }
+    }
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleKeyDown: React.KeyboardEventHandler<HTMLInputElement> = (event) => {
+        if (event.key === 'Enter') {
+            addTask();
+        }
+    }
+
+
+    function addTask() {
+        const newTask: Task = {
+            text: inputValue,
+            done: false,
+        }
+        updateTaskList([...taskList, newTask])
+        setInputValue('')
+        focusInput()
+    }
+
+    // Focus on the input field when mounted
+    useEffect(() => {
+        focusInput()
+    }, []);
 
     return (
         <>
             <h1>Task List</h1>
-            <form>
-                <input type='text' placeholder="Enter a new task"></input>
-                <button>Create</button>
-            </form>
+            <input type='text' placeholder="Enter a new task" value={inputValue} onChange={handleInputChange} onKeyDown={handleKeyDown} ref={inputRef} />
+            <button onClick={addTask} >Create</button>
             <div>
-                <p>task 1</p>
+                <ul>
+                    {taskList.map((task, index) => (
+                        <li key={index}>
+                            {task.text}
+                            {<button><img src={iconTrashCan} /></button>}
+                        </li>
+                    ))}
+                </ul>
             </div>
         </>
     )
